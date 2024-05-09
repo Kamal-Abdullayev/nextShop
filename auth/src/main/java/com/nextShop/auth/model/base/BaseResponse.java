2dad5338fd8fa18dad1c498c4a05f7467ab8bb0b
kamal-abdullayev <kamalabdu2layev@gmail.com>
2024-05-09 22:36:14 +0400

    feat/auth: liquibase & upgrade java version

--- auth/src/main/java/com/nextShop/auth/model/base/BaseResponse.java
+++ auth/src/main/java/com/nextShop/auth/model/base/BaseResponse.java
@@ -1,10 +1,15 @@
 package com.nextShop.auth.model.base;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseResponse<T> {
     HttpStatus status;
     String message;
@@ -18,36 +23,4 @@ public class BaseResponse<T> {
                 .build();
     }
 
-    public BaseResponse() {
-    }
-
-    public BaseResponse(HttpStatus status, String message, T data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
 }
